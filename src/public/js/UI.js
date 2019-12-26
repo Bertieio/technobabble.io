01d654323b6f7471e8fee3ecc8998e9ed7e0e346
Bertie <b@bertie.io>
2019-12-26 01:53:40 +0000

    Frontend work got social media icons working

--- src/public/js/UI.js
+++ src/public/js/UI.js
@@ -1,7 +1,39 @@
-$("#facebook").hover(function () {
-    console.log("test");
-    $("#footer").css("background-color", "#3b5998");
-}, function () {
-    console.log("test out");
-    $("#footer").css("background-color", "#424242");
+$(function () {
+    $("#facebook").hover(function () {
+        $("#footer").removeClass("footerDefault");
+        $("#footer").addClass("footerFB");
+        $('.socialText').html("Facebook:");
+    }, function () {
+        $("#footer").removeClass("footerFB");
+        $("#footer").addClass("footerDefault");
+        $('.socialText').html("Find us on:");
+    });
+    $("#twitter").hover(function () {
+        $("#footer").removeClass("footerDefault");
+        $("#footer").addClass("footerTW");
+        $('.socialText').html("Twitter:");
+    }, function () { 
+        $("#footer").removeClass("footerTW");
+        $("#footer").addClass("footerDefault");
+        $('.socialText').html("Find us on:");
+    });
+    $("#instagram").hover(function () {
+        $("#footer").removeClass("footerDefault");
+        $("#footer").addClass("footerIN");
+        $('.socialText').html("Instagram:");
+    }, function () {
+        $("#footer").removeClass("footerIN");
+        $("#footer").addClass("footerDefault");
+        $('.socialText').html("Find us on:");
+    });
+    $("#github").hover(function () {
+        $("#footer").removeClass("footerDefault");
+        $("#footer").addClass("footerGH");
+        $('.socialText').html("Github:");
+    }, function () {
+        $("#footer").removeClass("footerGH");
+        $("#footer").addClass("footerDefault");
+        $('.socialText').html("Find us on:");
+    });
+
 });
